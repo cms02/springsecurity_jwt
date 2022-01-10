17fa70d683b207d5639a203c09cec923a9b415a5
cms02 <minoui1234@gmail.com>
2022-01-10 15:23:39 +0900

    fix: import 수정

--- src/main/java/com/cms/jwt/config/SecurityConfig.java
+++ src/main/java/com/cms/jwt/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
